e9745a0e7151524a3044024145d38480cba592e5
Jesus <jesus.nunez2050@gmail.com>
2020-10-01 09:14:18 -0400

    Change if-else for Object Search

--- src/components/PokemonType.js
+++ src/components/PokemonType.js
@@ -1,50 +1,33 @@
 import React from "react";
 
-function PokemonType({ type }) {
-  const getClassName = () => {
-    if (type === "normal") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "fighting") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "flying ") {
-      return "badge badge-pill badge-info mx-1";
-    } else if (type === "poison") {
-      return "badge badge-pill badge-warning mx-1";
-    } else if (type === "ground") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "rock") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "ghost") {
-      return "badge badge-pill badge-dark mx-1";
-    } else if (type === "steel") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "fire") {
-      return "badge badge-pill badge-danger mx-1";
-    } else if (type === "water") {
-      return "badge badge-pill badge-primary mx-1";
-    } else if (type === "grass") {
-      return "badge badge-pill badge-success mx-1";
-    } else if (type === "electric") {
-      return "badge badge-pill badge-warning mx-1";
-    } else if (type === "psychic") {
-      return "badge badge-pill badge-light mx-1";
-    } else if (type === "dragon") {
-      return "badge badge-pill badge-danger mx-1";
-    } else if (type === "dark") {
-      return "badge badge-pill badge-dark mx-1";
-    } else if (type === "unknown") {
-      return "badge badge-pill badge-light mx-1";
-    } else if (type === "shadow") {
-      return "badge badge-pill badge-dark mx-1";
-    } else {
-      return "badge badge-pill badge-info mx-1";
-    }
-  };
+function PokemonType({type}) {
+    const getClassName = () => {
+        const pokemonTypes = {
+            "normal": "badge badge-pill badge-secondary mx-1",
+            "fighting": "badge badge-pill badge-secondary mx-1",
+            "flying": "badge badge-pill badge-info mx-1",
+            "poison": "badge badge-pill badge-warning mx-1",
+            "ground": "badge badge-pill badge-secondary mx-1",
+            "rock": "badge badge-pill badge-secondary mx-1",
+            "ghost": "badge badge-pill badge-dark mx-1",
+            "steel": "badge badge-pill badge-secondary mx-1",
+            "fire": "badge badge-pill badge-danger mx-1",
+            "water": "badge badge-pill badge-primary mx-1",
+            "grass": "badge badge-pill badge-success mx-1",
+            "electric": "badge badge-pill badge-warning mx-1",
+            "psychic": "badge badge-pill badge-light mx-1",
+            "dragon": "badge badge-pill badge-danger mx-1",
+            "dark": "badge badge-pill badge-dark mx-1",
+            "unknown": "badge badge-pill badge-light mx-1",
+            "shadow": "badge badge-pill badge-dark mx-1"
+        }
+        return pokemonTypes[type] ? pokemonTypes[type] : "badge badge-pill badge-info mx-1";
+    };
 
-  if (!type) {
-    return null;
-  }
-  return <span className={getClassName()}>{type}</span>;
+    if (!type) {
+        return null;
+    }
+    return <span className={getClassName()}>{type}</span>;
 }
 
 export default PokemonType;
