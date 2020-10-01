080cb354e958c40ad9b602dbddfbe96a83b08cdc
shardul <shardul.aeer@somaiya.edu>
2020-10-01 19:42:04 +0530

    [refactor] Remove if-else branching with object lookup
    Issue #13

--- src/components/PokemonType.js
+++ src/components/PokemonType.js
@@ -1,50 +1,35 @@
 import React from "react";
 
-function PokemonType({ type }) {
-  const getClassName = () => {
-    if (type === "normal") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "fighting") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "flying ") {
-      return "badge badge-pill badge-info mx-1";
-    } else if (type === "poison") {
-      return "badge badge-pill badge-warning mx-1";
-    } else if (type === "ground") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "rock") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "ghost") {
-      return "badge badge-pill badge-dark mx-1";
-    } else if (type === "steel") {
-      return "badge badge-pill badge-secondary mx-1";
-    } else if (type === "fire") {
-      return "badge badge-pill badge-danger mx-1";
-    } else if (type === "water") {
-      return "badge badge-pill badge-primary mx-1";
-    } else if (type === "grass") {
-      return "badge badge-pill badge-success mx-1";
-    } else if (type === "electric") {
-      return "badge badge-pill badge-warning mx-1";
-    } else if (type === "psychic") {
-      return "badge badge-pill badge-light mx-1";
-    } else if (type === "dragon") {
-      return "badge badge-pill badge-danger mx-1";
-    } else if (type === "dark") {
-      return "badge badge-pill badge-dark mx-1";
-    } else if (type === "unknown") {
-      return "badge badge-pill badge-light mx-1";
-    } else if (type === "shadow") {
-      return "badge badge-pill badge-dark mx-1";
-    } else {
-      return "badge badge-pill badge-info mx-1";
-    }
-  };
+const pokeTypes = {
+    normal: "badge-secondary",
+    fighting: "badge-secondary",
+    flying: "badge-info",
+    poison: "badge-warning",
+    ground: "badge-secondary",
+    rock: "badge-secondary",
+    ghost: "badge-dark",
+    steel: "badge-secondary",
+    fire: "badge-danger",
+    water: "badge-primary",
+    grass: "badge-success",
+    electric: "badge-warning",
+    psychic: "badge-light",
+    dragon: "badge-danger",
+    dark: "badge-dark",
+    unknown: "badge-light",
+    shadow: "badge-dark",
 
-  if (!type) {
-    return null;
-  }
-  return <span className={getClassName()}>{type}</span>;
+}
+function PokemonType({ type }) {
+    const getClassName = () => {
+        const badge = pokeTypes.hasOwnProperty(type) ? pokeTypes[type] : "badge-info";
+        return `badge badge-pill ${badge} mx-1`;
+    };
+
+    if (!type) {
+        return null;
+    }
+    return <span className={getClassName()}>{type}</span>;
 }
 
 export default PokemonType;
