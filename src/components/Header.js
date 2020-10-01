50e7af2bfe1f89f96b3984cd3c9a21aa71c33d81
shardul <shardul.aeer@somaiya.edu>
2020-10-01 19:15:14 +0530

    [feat] Material UI header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-function Header() {
-  return <div>header</div>;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function Header() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Pokemon react app
+                    </Typography>
+                    <Button color="inherit"
+                        onClick={() => window.open("https://github.com/Juggernaut9/pokemon", "_blank")}
+                    >Github</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-
-export default Header;
