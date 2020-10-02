2426db81b7afbfd728f0c95c7ac585c196af5a10
Bhavin789 <bhavib.789@gmail.com>
2020-10-02 19:27:37 +0530

    Add UT for Pokemon Type component

---
+++ src/tests/PokemonType.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import PokemonType from "../components/PokemonType";
+
+it("renders PokemonType component without crashing", () => {
+  const mockType = "normal";
+
+  const { asFragment, getByTestId } = render(<PokemonType type={mockType} />);
+  expect(asFragment()).toMatchSnapshot();
+  expect(getByTestId("pokemon-type-normal")).toHaveTextContent("normal");
+});
