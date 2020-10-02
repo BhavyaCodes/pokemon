efe0be9ce61574ecdfa8bf71b4348942da9ddfa6
Siddharth <siddharthmagadum16@gmail.com>
2020-10-02 22:59:42 +0530

    backgroundImage and css effects

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import Header from "./Header";
 import CardList from "./CardList";
 import Footer from './Footer'
+import Appcss from '../assets/css/App.css';
+// import Bgimage from '../assets/background-image.jpg'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={Appcss}>
       <Header />
       <div className="container">
         <CardList />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 import './index.css';
 
 ReactDOM.render(
@@ -14,4 +13,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
