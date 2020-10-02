7667f719f5aad19a4de7c841b0fe3e93d8b31bc6
Abdul Haseeb <haseeb.940@gmail.com>
2020-10-02 22:46:46 +0500

    Adds search feature in the top nav (#37)

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,7 @@ import React from "react";
 import pokemonList from "../pokemon.json";
 import PokeCard from "./PokeCard";
 
-function CardList() {
+function CardList({searchQuery}) {
   /**
    * This function sorts pokemons by their ids in ascending order
    */
@@ -24,7 +24,7 @@ function CardList() {
 
   return (
     <div className="row row-cols-1 row-cols-md-3 mt-3">
-      {sortedPokemonList.map((pokemon) => (
+      {sortedPokemonList.filter(pokemon => pokemon.name.includes(searchQuery)).map((pokemon) => (
         <PokeCard key={pokemon.id} pokemon={pokemon} />
       ))}
     </div>
