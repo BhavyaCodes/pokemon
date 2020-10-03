a78d6b69052f5e89f489833a89cf5ac87bcde9de
Bhavin789 <bhavib.789@gmail.com>
2020-10-03 13:43:53 +0530

    Update failing unit tests

--- src/tests/CardList.test.js
+++ src/tests/CardList.test.js
@@ -4,6 +4,6 @@ import { render } from "@testing-library/react";
 import CardList from "../components/CardList";
 
 it("renders CardList component without crashing", () => {
-  const { asFragment } = render(<CardList />);
+  const { asFragment } = render(<CardList searchQuery={"pika"} />);
   expect(asFragment()).toMatchSnapshot();
 });
