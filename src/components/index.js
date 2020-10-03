1519ca6bc8537cdf155a6286ef0a896bbe475bec
DheekshaGopika <dheekshagopikas@gmail.com>
2020-10-03 14:05:03 +0000

    Added index.js for easy importing files

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import App from "./App";
+import CardList from "./CardList";
+import Footer from "./Footer";
+import Header from "./Header";
+import PokeCard from "./PokeCard";
+import PokemonType from "./PokemonType";
+
+export {
+    App, CardList, Footer, Header, PokeCard, PokemonType
+}
