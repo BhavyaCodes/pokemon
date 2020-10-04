2d80e15a4b75e0fa54a1f239bfa5b2e042a40d91
Ashutosh Kumar <ashutoshkumarranchi1@gmail.com>
2020-10-05 02:33:04 +0530

    Pagination Done (#55)

---
+++ src/components/pagination.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Pagination = ({ currentPage, pageSize, setCurrentPage, totalData }) => {
+  return (
+    <nav>
+      <ul className="pagination">
+        {Array(Math.ceil(totalData / pageSize))
+          .fill(1)
+          .map((el, i) => (
+            <li
+              key={i + 1}
+              className={
+                i + 1 === currentPage ? "page-item active" : "page-item"
+              }
+            >
+              <a onClick={() => setCurrentPage(i + 1)} className="page-link">
+                {i + 1}
+              </a>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
