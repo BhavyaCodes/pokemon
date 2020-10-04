5dd1889c6e74851f919081412288df5d4903bc4f
Blastoise <ashutoshkumarranchi1@gmail.com>
2020-10-04 17:26:33 +0530

    Pagination Done

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import pokemonList from "../pokemon.json";
 import PokeCard from "./PokeCard";
+import Pagination from "./pagination";
 
 function CardList({ searchQuery }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(9);
+
   /**
    * This function sorts pokemons by their ids in ascending order
    */
@@ -21,17 +25,26 @@ function CardList({ searchQuery }) {
    * Refer this : https://www.learningjournal.guru/article/scala/functional-programming/immutability-in-functional-programming/
    */
   const sortedPokemonList = pokemonList.sort(pokemonComparator);
+  const filteredSortedPokemonList = sortedPokemonList.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
-    <div className="row row-cols-1 row-cols-md-3 mt-3">
-      {sortedPokemonList
-        .filter((pokemon) =>
-          pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-        .map((pokemon) => (
-          <PokeCard key={pokemon.id} pokemon={pokemon} />
-        ))}
-    </div>
+    <React.Fragment>
+      <div className="row row-cols-1 row-cols-md-3 mt-3">
+        {filteredSortedPokemonList
+          .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+          .map((pokemon) => (
+            <PokeCard key={pokemon.id} pokemon={pokemon} />
+          ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageSize={pageSize}
+        totalData={filteredSortedPokemonList.length}
+      />
+    </React.Fragment>
   );
 }
 
